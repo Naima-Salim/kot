c3e3bb38adf8d9c5b042ddd87537d999ba97cade
wambuingare <wambuinancyngare@gmail.com>
2022-03-10 08:42:15 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,31 @@
+fun main() {
+    campus()
+    var c=modulus(33, 3)
+    println(c)
+    var names=parameters("Naima", 21)
+    println(names)
+
+}
+
+fun campus() {
+    var campus = "codeHive"
+    var char5=[4])
+    var char6=[5]
+    var char7=[6]
+    var char8=[7]
+    println("$char5[4]$char6[5]$char7[6]$char8[7]")
+}
+
+fun modulus(a:Int, b:Int) {
+    var division=a%b
+    return modulus
+
+}
+
+fun parameters(name:String, age:Int):String {
+    var me = ("Hi, my name is $name and i am $age years old")
+}
+fun name(name: String){
+    var name = "Naima"
+    println(name.length)
+}
